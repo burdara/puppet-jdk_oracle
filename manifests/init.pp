f8e090b787ea5b63ca1c06f5df3e5b2ab862eb7f
Robbie Burda <robbieb@slalom.com>
2014-10-28 10:59:34 -0500

    unique names
    define type now requires resource names to be unique

--- manifests/init.pp
+++ manifests/init.pp
@@ -66,18 +66,18 @@ define jdk_oracle(
     $installerFilename = inline_template('<%= File.basename(@javaDownloadURI) %>')
 
     if ( $use_cache ){
-        notify { 'Using local cache for oracle java': }
+        notify { "Using local cache for oracle java ${version}": }
         file { "${install_dir}/${installerFilename}":
             source  => "puppet:///modules/jdk_oracle/${installerFilename}",
         }
-        exec { 'get_jdk_installer':
+        exec { "get_jdk_installer_${version}":
             cwd     => $install_dir,
             creates => "${install_dir}/jdk_from_cache",
             command => 'touch jdk_from_cache',
             require => File["${install_dir}/jdk-${version}-linux-x64.tar.gz"],
         }
     } else {
-        exec { 'get_jdk_installer':
+        exec { "get_jdk_installer_${version}":
             cwd     => $install_dir,
             creates => "${install_dir}/${installerFilename}",
             command => "wget -c --no-cookies --no-check-certificate --header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com\" --header \"Cookie: oraclelicense=accept-securebackup-cookie\" \"${javaDownloadURI}\" -O ${installerFilename}",
@@ -86,22 +86,22 @@ define jdk_oracle(
         }
         file { "${install_dir}/${installerFilename}":
             mode    => '0755',
-            require => Exec['get_jdk_installer'],
+            require => Exec["get_jdk_installer_${version}"],
         }
     }
 
     # Java 7/8 comes in a tarball so just extract it.
     if ( $version in [ '7', '8' ] ) {
-        exec { 'extract_jdk':
+        exec { "extract_jdk_${version}":
             cwd     => "${install_dir}/",
             command => "tar -xf ${installerFilename}",
             creates => $java_home,
-            require => Exec['get_jdk_installer'],
+            require => Exec["get_jdk_installer_${version}"],
         }
     }
     # Java 6 comes as a self-extracting binary
     if ( $version == '6' ) {
-        exec { 'extract_jdk':
+        exec { "extract_jdk_${version}":
             cwd     => "${install_dir}/",
             command => "${install_dir}/${installerFilename}",
             creates => $java_home,
@@ -110,30 +110,33 @@ define jdk_oracle(
     }
 
     # Set links depending on osfamily or operating system fact
-    file { "${install_dir}/java_home":
+    file { "setup_java_home_${version}":
+        path    => "${install_dir}/java_home",
         ensure  => link,
         target  => $java_home,
-        require => Exec['extract_jdk'],
+        require => Exec["extract_jdk_${version}"],
     }
     file { "${install_dir}/jdk-${version}":
         ensure  => link,
         target  => $java_home,
-        require => Exec['extract_jdk'],
+        require => Exec["extract_jdk_${version}"],
     }
     case $::osfamily {
         RedHat: {
             exec { ["/usr/sbin/alternatives --install /usr/bin/java java ${java_home}/bin/java 20000",
                     "/usr/sbin/alternatives --install /usr/bin/javac javac ${java_home}/bin/java 20000"]:
-                require => Exec['extract_jdk'],
+                require => Exec["extract_jdk_${version}"],
             } ->
             exec { ["/usr/sbin/alternatives --set java ${java_home}/bin/java",
                     "/usr/sbin/alternatives --set javac ${java_home}/bin/java"]:
             } ->
-            file { 
-                '/usr/sbin/java':
+            file {
+                "sbin_java_${version}":
+                    path    => '/usr/sbin/java',
                     ensure  => link,
                     target  => '/etc/alternatives/java';
-                '/usr/sbin/javac':
+                "sbin_javac_${version}":
+                    path    => '/usr/sbin/javac',
                     ensure  => link,
                     target  => '/etc/alternatives/javac';
             }
@@ -141,16 +144,18 @@ define jdk_oracle(
         Debian, Suse:    {
             exec { ["/usr/sbin/update-alternatives --install /usr/bin/java java ${java_home}/bin/java 20000",
                     "/usr/sbin/update-alternatives --install /usr/bin/javac javac ${java_home}/bin/javac 20000"]:
-                require => Exec['extract_jdk'],
+                require => Exec["extract_jdk_${version}"],
             } ->
             exec { ["/usr/sbin/update-alternatives --set java ${java_home}/bin/java",
                     "/usr/sbin/update-alternatives --set javac ${java_home}/bin/javac"]:
             } ->
-            file { 
-                '/usr/sbin/java':
+            file {
+                "sbin_java_${version}":
+                    path    => '/usr/sbin/java',
                     ensure  => link,
                     target  => '/etc/alternatives/java';
-                '/usr/sbin/javac':
+                "sbin_javac_${version}":
+                    path    => '/usr/sbin/javac',
                     ensure  => link,
                     target  => '/etc/alternatives/javac';
             }
